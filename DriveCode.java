022c4319584d7090231e33f2b68753e3eb3ba5ab
Enrj-Drunk <99850420+Imaginit22@users.noreply.github.com>
2024-12-05 01:00:23 -0500

    Update DriveCode.java

--- DriveCode.java
+++ DriveCode.java
@@ -73,7 +73,7 @@ public class DriveCode extends LinearOpMode {
         backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         manager = new MotorManager([
-            MotorTPR("StudicaMotor", 356),
+            MotorTPR("StudicaMotor", 366),
             MotorTPR("HexMotor", 560),
             MotorTPR("CoreHexMotor", 288)
         ]);
