cd671d1a653f9eed168b268089ef9541e11b4238
DVSS-STEM <dvss.stem@outlook.com>
2024-12-09 11:14:24 -0500

    ow

--- DriveCode.java
+++ DriveCode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 //imports
     //eventloop components
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -33,7 +34,7 @@ public class DriveCode extends LinearOpMode {
 
     DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor, vertical_1, vertical_2, horizontal, armMotor;
 
-    MotorManager manager;
+
 
     @Override
     public void runOpMode(){
@@ -42,50 +43,33 @@ public class DriveCode extends LinearOpMode {
         backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-        vertical_1 = hardwareMap.dcMotor.get("Vertical 1");
-        vertical_2 = hardwareMap.dcMotor.get("Vertical 2");
-        horizontal = hardwareMap.dcMotor.get("Horizontal");
         armMotor = hardwareMap.dcMotor.get("armMotor");
         
-        sub_peck = hardwareMap.crservo.get("peck");
-        specimen_claw = hardwareMap.servo.get("specimen claw");
-        sample_claw = hardwareMap.servo.get("sample claw");
-        sub_rotation = hardwareMap.servo.get("rotation");
 
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        specimen_claw.setDirection(Servo.Direction.REVERSE);
-        sample_claw.setDirection(Servo.Direction.REVERSE);
-        sub_rotation.setDirection(Servo.Direction.REVERSE);
         
         //set zero power behaviour to brake
         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        vertical_1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        vertical_2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        horizontal.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         //sub_peck.setZeroPowerBehavior(CrServo.ZeroPowerBehavior.BRAKE);
         frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        manager = new MotorManager([
-            MotorTPR("StudicaMotor", 366, 112),
-            MotorTPR("HexMotor", 560, 300),
-            MotorTPR("CoreHexMotor", 288, 125)
-        ]);
-       waitForStart();
+        MotorManager manager = new MotorManager();
+        waitForStart();
 
         while (opModeIsActive()) {
             manager.stopCompleted();
-            telemetry.addData("----ACTIVE MOTORS----");
+            telemetry.addData("----ACTIVE MOTORS----", "");
             for (DcMotor motor : manager.getPoweredMotors()) {
                 telemetry.addData("Motor Name", motor.getDeviceName());
                 telemetry.addData("Ticks remaining", motor.getTargetPosition() - motor.getCurrentPosition());
-                telemetry.addData(",");
+                telemetry.addData(",", "");
             }
             double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
@@ -102,38 +86,16 @@ public class DriveCode extends LinearOpMode {
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
             
-            flmtype = frontLeftMotor.getMotorType();
-            telemetry.addData("FLMTYPENAME", flmtype.name)
-            telemetry.addData("FLMTYPETPM", flmtype.getTicksPerRev())
-            if (gamepad1.dpad_right){
-                manager.run(horizontal, 12, 1);
-            } else if (gamepad1.dpad_left){
-                manager.run(horizontal, -12, 1);
-            }
+            MotorConfigurationType flmtype = armMotor.getMotorType();
+            telemetry.addData("FLMTYPENAME", flmtype.getName());
+            telemetry.addData("FLMTYPETPM", flmtype.getTicksPerRev());
             
             if (gamepad1.right_bumper){
-                manager.run(armMotor, 6, 1);
+                manager.runTicks(armMotor, 6, 1);
             } else if (gamepad1.left_bumper){
-                manager.run(armMotor, -6, 1);
+                manager.runTicks(armMotor, -6, 1);
             }
             
-            if (gamepad1.right_trigger>0.3){
-                manager.run(sub_peck, 12, 1);
-            } else if (gamepad1.left_trigger>0.3){
-                manager.run(sub_peck, -12, 1);
-            }
-            
-            if (gamepad1.a){
-                specimen_claw.setPosition(0);
-            } else if (gamepad1.b){
-                specimen_claw.setPosition(0.4);
-            }
-            
-            if (gamepad1.x){
-                sample_claw.setPosition(0);
-            } else if (gamepad1.y){
-                sample_claw.setPosition(0.4);
-            }
             
             telemetry.update();
             
@@ -141,6 +103,70 @@ public class DriveCode extends LinearOpMode {
         }
     }
     
+    private class MotorTPR {
+        String type;
+        int tpr;
+        int rpm;
+        public MotorTPR(String type, int tpr, int rpm) {
+            this.type = type;
+            this.tpr = tpr;
+            this.rpm = rpm;
+        }
+        public String getType() {
+            return type;
+        }
+        public int getTPR() {
+            return tpr;
+        }
+        public int getRPM() {
+            return rpm;
+        }
+    }
+    private class MotorManager {
+        private ArrayList<DcMotor> poweredMotors = new ArrayList<>();
+        //ticks are how much it runs after a single press, basically
+        //a lower threshold value for movement.
+        public void switchOn(DcMotor motor) {
+            poweredMotors.add(motor);
+        }
+        public void runTicks(DcMotor motor, int ticks, double power) {
+            if (!motor.isBusy()) {
+                switchOn(motor);
+                motor.setTargetPosition(ticks);
+                motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                motor.setPower(power);
+            }
+        }
+        public void resetTicks(DcMotor motor) {
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        }
+        public void stopMotor(DcMotor motor) {
+            motor.setPower(0);
+            poweredMotors.remove(motor);
+        }
+        public void stopAndReset(DcMotor motor) {
+            resetTicks(motor);
+            stopMotor(motor);
+        }
+        public void stopCompleted() {
+            for (DcMotor motor : getPoweredMotors()) {
+                if (!motor.isBusy()) {
+                    stopAndReset(motor);
+                }
+            }
+        }
+        public ArrayList<DcMotor> getPoweredMotors() {
+            return new ArrayList<>(poweredMotors);
+        }
+    }
+    
+    //useful links
+    /*
+     * https://javadoc.io/doc/org.firstinspires.ftc
+     * DCMOTOR DOCUMENTATION (has MotorConfigurationType)
+     * https://javadoc.io/doc/org.firstinspires.ftc/RobotCore/latest/com/qualcomm/robotcore/hardware/configuration/typecontainers/MotorConfigurationType.html
+     */
 
     
 }
+
